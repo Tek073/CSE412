b5ceb2cd3cb2b3dc7460fd6b1f28b8b5ad7cc011
Ryan Pettinato <99558651+Tek073@users.noreply.github.com>
2022-10-26 19:06:01 -0700

    Create abilities.sql

---
+++ abilities.sql
@@ -0,0 +1,23 @@
+-- create abilities table
+CREATE TABLE abilites (
+  name varchar(55) NOT NULL,
+  text varchar(55),
+  type varchar(55);
+  PRIMARY KEY (name)
+);
+
+-- fill abilities table
+INSERT INTO abilities
+  ('harden', 'increase pokemons defense', 'normal'),
+  ('charm', 'lower enemy attack', 'fairy'),
+  ('leer', 'lower enemy defense', 'normal'),
+  ('double team', 'increase evade stat', 'normal'),
+  ('sharpen', 'increase special attack', 'normal'),
+  ('confusion', 'inflict confusion', 'psychic'),
+  ('hail', 'increase ice attacks', 'ice'),
+  ('sunny day', 'increase fire attacks', 'fire'),
+  ('water sprout', 'increase water attacks', 'water'),
+  ('toxic thread', 'poison enemy', 'poison');
+  
+-- show abilities table
+SELECT * FROM abilities;
