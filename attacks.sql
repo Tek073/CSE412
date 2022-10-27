62057844ac66341022fd97ddc69692de9c3a14d1
Ryan Pettinato <99558651+Tek073@users.noreply.github.com>
2022-10-26 18:54:24 -0700

    Update attacks.sql

--- attacks.sql
+++ attacks.sql
@@ -1,3 +1,4 @@
+-- create table attacks
 CREATE TABLE attacks (
   name varchar(55) NOT NULL,
   cost int,
@@ -6,9 +7,17 @@ CREATE TABLE attacks (
   PRIMARY KEY (name)
 );
 
-INSERT INTO attacks
-VALUES ('tackle', 2, 'charge the opponent', 20);
-VALUES ('pound', 2, 'pound the pokemon', 30);
-VALUES ('scratch', 1, 'scratch the pokemon', 25);
+-- fill table attacks
+INSERT INTO attacks 
+VALUES 
+  ('tackle', 2, 'charge the opponent', 20),
+  ('pound', 2, 'pound the pokemon', 30),
+  ('scratch', 1, 'scratch the pokemon', 25),
+  ('bite', 3, 'bite the pokemon', 60),
+  ('blizzard', 5, 'create a blizzrd', 110),
+  ('ember', 2, 'burn enemy', 40),
+  ('cut', 1, 'cut pokemon', 20),
+  ('double kick', 4, 'double kick pokemon', 75);
 
+-- show that attacks table is full
 SELECT * FROM attacks;
