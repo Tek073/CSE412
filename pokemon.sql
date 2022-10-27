5b8aec34508094e38c13dc49b385f373be04d43d
Ryan Pettinato <99558651+Tek073@users.noreply.github.com>
2022-10-27 13:14:41 -0700

    Update pokemon.sql

--- pokemon.sql
+++ pokemon.sql
@@ -6,6 +6,11 @@ CREATE TABLE pokemon (
   evolvesTo varchar(55),
   evolvesFrom varchar(55),
   nameAbility varchar(55),
+  nameAttack varchar(55),
+  typeWeaknesses varchar(55),
+  valueWeaknesses int,
+  typeResistances varchar(55),
+  valueResistances int,
   PRIMARY KEY (pxdx),
   FOREIGN KEY (nameAbility) REFERENCES abilities(nameAbility),
   FOREIGN KEY (nameAttack) REFERENCES attacks(nameAttack),
@@ -15,6 +20,6 @@ CREATE TABLE pokemon (
 
 INSERT INTO pokemon
 VALUES
-  ('biddof', 12, 33, 1, 'barbarrel', 'n/a', 'harden');
+  ('biddof', 12, 33, 1, 'barbarrel', 'n/a', 'harden', 'pound', 'fire', 2, 'water', 2);
   
 SELECT * FROM pokemon
