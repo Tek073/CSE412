d68271ab05d22ee3f0d36c75894c9570e25ab8ea
LucTran-git <114346146+LucTran-git@users.noreply.github.com>
2022-10-28 16:36:12 -0700

    Update resistances.sql

--- resistances.sql
+++ resistances.sql
@@ -1,23 +1,26 @@
 -- create resistance table
 CREATE TABLE resistances (
+  id varchar(20),
   typeResistances varchar(55),
-  valueResistances int,
+  valueResistances varchar(5), -- e.g. 0.5
   PRIMARY KEY (typeResistances, valueResistances)
+  FOREIGN KEY (id) REFERENCES pokemon(id)
 );
 
--- insert values into table
-INSERT INTO resistances
-VALUES
-  ('water', 2),
-  ('fire', 2),
-  ('grass', 2),
-  ('fighting', 2),
-  ('rock', 1),
-  ('ground', 1),
-  ('dark', 1),
-  ('flying', 1),
-  ('ice', 1),
-  ('bug', 2);
+-- resistances added simultaneously with card info
+-- -- insert values into table
+-- INSERT INTO resistances
+-- VALUES
+--   ('water', 2),
+--   ('fire', 2),
+--   ('grass', 2),
+--   ('fighting', 2),
+--   ('rock', 1),
+--   ('ground', 1),
+--   ('dark', 1),
+--   ('flying', 1),
+--   ('ice', 1),
+--   ('bug', 2);
   
--- show resistances table is filled
-SELECT * FROM resistances
+-- -- show resistances table is filled
+-- SELECT * FROM resistances
