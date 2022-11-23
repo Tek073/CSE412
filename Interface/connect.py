21468e621d0b3f631439f08b1bd4cb9bfe7d6386
LucTran-git <114346146+LucTran-git@users.noreply.github.com>
2022-11-23 01:21:38 -0700

    reorganize files

--- Interface/connect.py
+++ Interface/connect.py
@@ -1,20 +1,34 @@
 import psycopg2
 from psycopg2 import Error
-from decks import *
+from psycopg2 import *
+from user_items import *
+
+DB_USER = "postgres"
+DB_PASS = "asdf"
 
 DB_HOST = "localhost"
 DB_PORT = "5432"
 
-class Connection:
-    def __init__(user, DB_USER, DB_PASS): # this is basically user login
-        user.connection = psycopg2.connect( user=DB_USER,
-                                            password=DB_PASS, 
-                                            host=DB_HOST, 
-                                            port=DB_PORT, 
-                                            database='PTCG_DB') # we are NOT going to use separate DB for users.
-                                                            # searching online, apparently it's kind of a bad idea
-        user.cursor = user.connection.cursor()
-        user.deck = Deck(user)
+class DBConnection:
+    def __init__(user, username, password): 
+        conn = connect( 
+            user=DB_USER,
+            password=DB_PASS, 
+            host=DB_HOST, 
+            port=DB_PORT, 
+            database='postgres') # one DB for all users
+
+        user.cursor = conn.cursor()
+        user.cursor.execute('''
+            SELECT userID
+            FROM users
+            WHERE username = %s AND password = %s;''',
+            (username, password)
+        )
+        user.id = user.cursor.fetchone()[0]
+        user.collection = Collection(user.id, conn)
+        user.decks = Deck(user.id, conn)
+        
 
     def logout(user):
         user.cursor.close()
--- Interface/database.py
+++ Interface/database.py
@@ -1,4 +1,8 @@
 import sys
+
+from flask import Flask
+from markupsafe import escape
+
 import psycopg2
 from psycopg2 import Error
 
@@ -8,9 +12,10 @@ from pokemontcgsdk import Type
 from pokemontcgsdk import Supertype
 from pokemontcgsdk import Subtype
 from pokemontcgsdk import Rarity
-
 from pokemontcgsdk import RestClient
-RestClient.configure('') # Your API key goes here
+RestClient.configure('92e6be5e-7d21-4d3e-9900-1753c97c0979') # Your API key goes here
+
+from connect import *
 
 DB_USER = "postgres"
 DB_PASS = "asdf"
@@ -18,41 +23,79 @@ DB_HOST = "localhost"
 DB_PORT = "5432"
 DB_NAME = "ptcg_db"
 
-from connect import *
-from decks import *
+# This was a test to get card info
 
-try:
-    # This was a test to get card info
-    original_stdout = sys.stdout # Save a reference to the original standard output
-    with open('filename.txt', 'w') as f:
-        sys.stdout = f # Change the standard output to the file we created.
-        card = Card.find('xy1-1')
-        print(card)
-        sys.stdout = original_stdout # Reset the standard output to its original value
-        exit
+#original_stdout = sys.stdout # Save a reference to the original standard output
+# with open('pkmn_w_ability.txt', 'w') as f:
+#     sys.stdout = f # Change the standard output to the file we created.
+#     card = Card.find('bw1-6')1
+#     #getNewSet('xy1')
+#     print(card)
+#     sys.stdout = original_stdout # Reset the standard output to its original value
+#     exit
 
-    #input("New user, or returning user?")
-    username = input("Username: ")
-    password = input("Passsword: ")
+# ancient traits use special chars like α, which require utf-8 (as opposed to default ascii)
+# with open('pkmn_w_ancientTrait.txt', 'w', encoding='utf-8') as f:
+#     sys.stdout = f # Change the standard output to the file we created.
+#     card = Card.find('xy5-104')
+#     #getNewSet('xy1')
+#     print(card)
+#     sys.stdout = original_stdout # Reset the standard output to its original value
+#     exit
 
-    user = Connection(username, password)
+user = DBConnection('asdf', 'asdf') # user in 'users' table; NOT the Database user, which will be the same for everyone
 
-    option = input("""Type in # of option you want, or q to quit
-                        1: Add card to collection (ac)
-                        2: Add card to deck (ad)
-                        q: quit (q)\n""")
+str = 'asdf, adsf, asdf'
+asdf = user.cursor.mogrify('''
+    INSERT INTO sets
+    VALUES (%s);''',
+    (str,) # psycopg2 tries to index into input, i.e. iterate over it. So we turn it into a tuple to allow this.
+)
+user_id = user.id
+print(user.id)
 
-    if option == 1:
-        print("ac")
-    elif option == 2:
-        print("ad")
-    elif option == 'q':
-        print("quit")
+# try:
 
-except (Exception, Error) as error:
-    print("Error while connecting to PostgreSQL: ", error)
+#     #input("New user, or returning user?")
 
-finally:
-    if (user.connection):
-        user.logout()
-        print("PostgreSQL connection is closed")
+#     username = input("Username: ")
+#     password = input("Passsword: ")
+
+#     user = DBConnection(username, password)
+
+#     option = input("""Type in # of option you want, or q to quit
+#                         1: Add card to collection (ac)
+#                         2: Add card to deck (ad)
+#                         q: quit (q)\n""")
+
+#     if option == 1:
+#         # todo: search by other attributes
+#         cardID = input('cardID: ')
+#         user.addToCollection(cardID)
+#         #print("ac")
+#     elif option == 2:
+#         # todo: search by other attributes
+#         deckID = input('deckID: ')
+#         cardID = input('cardID: ')
+#         user.addToDeck(deckID, cardID)
+#         #print("ad")
+#     elif option == 'q':
+#         print("quit")
+
+# except (Exception, Error) as error:
+#     print("Error while connecting to PostgreSQL: ", error)
+
+# finally:
+#     if (user.connection):
+#         user.logout()
+#         print("PostgreSQL connection is closed")
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "<p>Pokemon Trading Card Gallery!!!!!!!!!!!!!!!!!!!!!!</p>"
+
+@app.route("/<user_id>/decks")
+def decks(user_id = user.id, deck_id = user.decks):
+    return "<p>decks</p>"
