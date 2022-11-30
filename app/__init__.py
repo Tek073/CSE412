d067417777d2b2f0eda8f176f85deb03e03835b6
Abraham Martinez <aimarti9@asu.edu>
2022-11-30 00:27:29 -0700

    Search cards and add to Collection

--- app/__init__.py
+++ app/__init__.py
@@ -11,11 +11,13 @@ def create_app():
     from .login import login
     from .signUp import signUp
     from .cardCollection import cardCollection
+    from .addToCollection import addToCollection
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(decks, url_prefix='/')
     app.register_blueprint(deckManager, url_prefix='/')
     app.register_blueprint(login, url_prefix='/login')
     app.register_blueprint(signUp, url_prefix='/sign_up')
     app.register_blueprint(cardCollection, url_prefix='/cardCollection')
+    app.register_blueprint(addToCollection, url_prefix='/addToCollection')
 
     return app
---
+++ app/addToCollection.py
@@ -0,0 +1,46 @@
+from flask import Blueprint, render_template, flash, request, redirect
+from globals import search_cards
+
+addToCollection = Blueprint('addToCollection', __name__)
+
+@addToCollection.route('/', methods=['GET', 'POST'])
+def begin():
+    from start import user
+    userCollection = []
+    results = []
+    username = user.username
+    print(username)
+    collection = user.collection.get()
+
+
+    return render_template('addToCollection.html')
+
+@addToCollection.route('/add/<cardID>', methods=['GET', 'POST'])
+def add(cardID):
+    from start import user
+
+    user.cursor.execute('''
+        INSERT INTO _cards_in_collections VALUES
+        (%s, %s, %s)''',
+        (user.id, cardID, 1))
+    user.conn.commit()
+
+    return redirect('/addToCollection')
+
+@addToCollection.route('/search', methods=['GET', 'POST'])
+def search(): 
+    from start import user
+    userCollection = []
+    if request.method == "POST":
+        cardName = request.form['cardName']
+        print(cardName)
+        user.cursor.execute('''
+            SELECT cardID, name, largeImage, types
+            FROM cards
+            WHERE upper(name) LIKE upper(%s)''',
+            [cardName])
+        
+        userCollection.append(user.cursor.fetchone())
+        print(userCollection)
+    return render_template('addToCollection.html', username=user.username, cards=userCollection)
+    
